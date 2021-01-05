c4ed878a73ab4ed9cd947f621d05b82e6159cf8d
Hemant Sharma <hemantbeast@gmail.com>
2021-01-05 10:57:03 +0530

    Initial commit

---
+++ DeliveryHtml.iOS/AppDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Foundation;
+using LabelHtml.Forms.Plugin.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace DeliveryHtml.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			HtmlLabelRenderer.Initialize();
+			Forms.Init();
+			LoadApplication(new App());
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
---
+++ DeliveryHtml/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DeliveryHtml
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
